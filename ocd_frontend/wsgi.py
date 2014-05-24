6ca650ace76c18cfffeaf8cd8b3d14d878df3d9d
Bart de Goede <bart@dispectu.com>
2014-05-24 16:16:35 +0200

    Removed api

--- ocd_frontend/wsgi.py
+++ ocd_frontend/wsgi.py
@@ -3,5 +3,5 @@ from werkzeug.wsgi import DispatcherMiddleware
 from ocd_frontend import rest
 
 application = DispatcherMiddleware(rest.create_app(), {
-    '/api/v0': rest.create_app()
+    '/v0': rest.create_app()
 })
