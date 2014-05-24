159b51c7a830b5a27a100d06f06f81dc03524694
Justin van Wees <justin@vwees.net>
2014-05-24 23:09:37 +0200

    Import local_settings in ocd_frontend

--- ocd_frontend/settings.py
+++ ocd_frontend/settings.py
@@ -77,3 +77,11 @@ AVAILABLE_FACETS = {
 ALLOWED_DATE_INTERVALS = ('day', 'week', 'month', 'quarter', 'year')
 
 RESOLVER_URL_INDEX = 'ocd_resolver'
+
+# Allow any settings to be defined in local_settings.py which should be
+# ignored in your version control system allowing for settings to be
+# defined per machine.
+try:
+    from local_settings import *
+except ImportError:
+    pass
