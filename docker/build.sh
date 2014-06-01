768873ecf146cd75efdc827d6d5b5da378fa771d
Lex Slaghuis <lex@wikiwise.nl>
2014-06-01 17:34:22 +0200

    Update of Docker.io to make ocd-api happen

--- docker/build.sh
+++ docker/build.sh
@@ -5,22 +5,24 @@
 
 #Build Elastic Search
 IMAGE=i-es
-#sudo docker build -t $IMAGE github.com/orchardup/docker-elasticsearch
 sudo docker build -t $IMAGE github.com/ajslaghu/docker-elasticsearch
-#https://github.com/ajslaghu/docker-elasticsearch.git
 
 #Build Redis
 IMAGE=i-redis
-sudo docker build -t=$IMAGE github.com/dockerfile/redis
+sudo docker build -t $IMAGE github.com/dockerfile/redis
 
 #Build OCD Python, for celery (transformers) and cmd (extractors)
 IMAGE=i-ocd-python
-sudo sudo docker build -t $IMAGE ocd-python/.
+sudo docker build -t $IMAGE ocd-python/.
+
+#Build Nginx / Python webApp for the API
+IMAGE=i-ocd-api
+sudo docker build -t $IMAGE ocd-api/.
 
 echo
 echo Installation finished
 echo To Do stuff to run the API
 echo Start the stack with
 echo "sh run.sh"
-echo and continue with loading by
+echo and continue with Loading data with:
 echo 'sh run-ocd-cmd.sh' 
--- docker/run.sh
+++ docker/run.sh
@@ -2,8 +2,7 @@
 # prerequisites
 # Test installation 
 
-sudo docker stop c-es c-redis c-ocd-celery
-sudo docker rm c-es c-redis c-ocd-celery
+sudo docker rm -f c-es c-redis c-ocd-celery c-ocd-api
 
 #Run Elastic Search as a single daemon, persistance by file mount on host
 IMAGE=i-es
@@ -13,12 +12,13 @@ LOCALPATH=/var/lib/elasticsearch
 mkdir -p $HOSTPATH
 sudo docker run --name $CONTAINER -v $HOSTPATH:$LOCALPATH:rw \
  -t -i -d $IMAGE
-
+ESIP=$(docker inspect $CONTAINER | grep IPAddress | cut -d '"' -f 4)
 
 #Run Redis, inpersistant
 IMAGE=i-redis
 CONTAINER=c-redis
 sudo docker run --name $CONTAINER -t -i -d $IMAGE
+REDISIP=$(docker inspect $CONTAINER | grep IPAddress | cut -d '"' -f 4)
 
 #Run Celery OCD Worker
 IMAGE=i-ocd-python
@@ -28,16 +28,47 @@ LOCALPATH=/mnt/data
 LINK="--link c-redis:redis"
 LINK2="--link c-es:es"
 RUNCMD="/bin/sh /start-celery.sh"
+
+STR="CELERY_CONFIG = {\n   'BROKER_URL': 'redis://$REDISIP:6379/0', \n   'CELERY_RESULT_BACKEND': 'redis://$REDISIP:6379/0' \n}\nELASTICSEARCH_HOST = '$ESIP'\n"
+#ELASTICSEARCH_PORT = 9200 \n "
+echo $STR > $HOSTPATH/ocd_backend/local_settings.py
+
 sudo docker run $LINK $LINK2 --name $CONTAINER \
 -v $HOSTPATH:$LOCALPATH:rw  -t -i -d $IMAGE $RUNCMD
 
-sleep 10
+sleep 30
+#implement ES ready monitor
+#curl es:9200/somthin and parse it
+#error: "Read-only file system" setting key "vm.max_map_count"
+# "started"
 # install template on Elastic Search
 CONTAINER=tmp
 sudo docker run $LINK $LINK2  --name $CONTAINER \
 -v $HOSTPATH:$LOCALPATH:rw  \
 -t -i $IMAGE /bin/sh -c "cd /mnt/data && ./manage.py elasticsearch put_template"
 sudo docker rm $CONTAINER
+
+# INSERT API STUFF
+ES=c-es
+IMAGE=i-ocd-api
+CONTAINER=c-ocd-api
+HOSTPATH=$(pwd)/..
+LOCALPATH=/mnt/data
+LINK2="--link c-es:es"
+EXPOSE="-p 80:80"
+DAEMON=-d
+
+echo $ESIP
+# Script that updates local_settings.py in the ocd_frontend directory
+echo "ELASTICSEARCH_HOST = '$ESIP' \n " > $HOSTPATH/ocd_frontend/local_settings.py
+docker run $EXPOSE $LINK2 -v $HOSTPATH:$LOCALPATH  --name $CONTAINER -t -i $DAEMON $IMAGE
+APIIP=$(docker inspect $CONTAINER | grep IPAddress | cut -d '"' -f 4)
+echo $APIIP
+sleep 5
+curl -XPOST http://$APIIP/v0/search  -d '{"query": "fiets"}'
+curl -XPOST http://localhost/v0/search  -d '{"query": "fiets"}'
+
+
 
 
 echo
