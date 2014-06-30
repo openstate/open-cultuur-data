e450c00570b1b69634d9246b4911517ba67e0478
Justin van Wees <justin@vwees.net>
2014-06-30 15:03:22 +0200

    Changes to ZoutkampItem:
    - Removed obsolete namespaces
    - Store multiple authors, when present
    - Include all items in the 'all_text' field
    
    According to the documentation there should be a lot more info available
    through the API, but field such as description, date and keyword are not
    exposed. I will make an issue for this.

--- ocd_backend/items/zoutkamp.py
+++ ocd_backend/items/zoutkamp.py
@@ -1,30 +1,18 @@
-from datetime import datetime
-
 from ocd_backend.items import BaseItem
 
 class ZoutkampItem(BaseItem):
-    namespaces = {
-        'oai': 'http://www.openarchives.org/OAI/2.0/',
-        'dc': 'http://purl.org/dc/elements/1.1/',
-        'oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/',
-        'xml': 'http://www.w3.org/XML/1998/namespace'
-    }
+    def _get_all_text(self, xpath_expression):
+        nodes = self.original_item.findall(xpath_expression)
 
-    media_mime_types = {
-        'webm': 'video/webm',
-        'ogv': 'video/ogg',
-        'ogg': 'audio/ogg',
-        'mp4': 'video/mp4',
-        'm3u8': 'application/x-mpegURL',
-        'ts': 'video/MP2T',
-        'mpeg': 'video/mpeg',
-        'mpg': 'video/mpeg',
-        'png': 'image/png',
-        'jpg': 'image/jpg',
-    }
+        texts = []
+        for node in nodes:
+            if node.text is not None:
+                texts.append(unicode(node.text))
+
+        return texts
 
     def _get_text_or_none(self, xpath_expression):
-        node = self.original_item.find(xpath_expression, namespaces=self.namespaces)
+        node = self.original_item.find(xpath_expression)
         if node is not None and node.text is not None:
             return unicode(node.text)
 
@@ -37,7 +25,7 @@ class ZoutkampItem(BaseItem):
         original_id = self.get_original_object_id()
         return {
             'html': 'http://maritiemdigitaal.nl/index.cfm?event=search.getdetail&id=%s' % original_id,
-            'xml': 'http://mmr.adlibhosting.com/madigopacx/wwwopac.ashx?database=ChoiceMardig&search=priref=%s&limit=10&xmltype=unstructured' % original_id
+            'xml': 'http://mmr.adlibhosting.com/madigopacx/wwwopac.ashx?database=ChoiceMardig&search=priref=%s&xmltype=unstructured' % original_id
         }
 
     def get_collection(self):
@@ -52,47 +40,32 @@ class ZoutkampItem(BaseItem):
         title = self._get_text_or_none('.//title')
         combined_index_data['title'] = title
 
-        description = self._get_text_or_none('.//description')
-        if description:
-            combined_index_data['description'] = description
-
-        authors = self._get_text_or_none('.//creator')
+        authors = self._get_all_text('.//creator')
         if authors:
-            combined_index_data['authors'] = [authors]
+            combined_index_data['authors'] = authors
 
-        mediums = self.original_item.findall('.//image',
-                                              namespaces=self.namespaces) # always jpg
-
+        mediums = self.original_item.findall('.//image')
         if mediums is not None:
             combined_index_data['media_urls'] = []
 
             for medium in mediums:
                 combined_index_data['media_urls'].append({
                     'original_url': medium.text,
-                    'content_type': self.media_mime_types[medium.text.split('.')[-1]]
+                    'content_type': 'image/jpg'
                 })
 
         return combined_index_data
 
     def get_index_data(self):
-        invno = self._get_text_or_none('.//invno')
-        return {
-            'invno': invno
-        }
+        return {}
 
     def get_all_text(self):
         text_items = []
 
         # Title
-        text_items.append(self._get_text_or_none('.//title'))
+        text_items += self._get_all_text('.//title')
 
         # Creator
-        text_items.append(self._get_text_or_none('.//creator'))
-
-        # Description
-        text_items.append(self._get_text_or_none('.//dc:description'))
-
-        # Publisher
-        text_items.append(self._get_text_or_none('.//source'))
+        text_items = self._get_all_text('.//creator')
 
         return u' '.join([ti for ti in text_items if ti is not None])
