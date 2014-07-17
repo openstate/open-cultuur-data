37428427938f2b235c47ecdca5b14c1981b5b8e8
Justin van Wees <justin@vwees.net>
2014-07-17 16:07:10 +0200

    Changes to TropenMuseumItem:
    - Removed unnecessary namespaces
    - Don't store date if not present
    - Replaced references to Tropen Museum website with Wikipedia URL's
      because the Tropen Museum URL construction was not reliable
    - Descriptions are now also extracted with the help of BeautifulSoup
    - Removed unused imports

--- ocd_backend/items/tropenmuseum.py
+++ ocd_backend/items/tropenmuseum.py
@@ -1,21 +1,19 @@
 import re
-import urllib
-from lxml import etree
+from BeautifulSoup import BeautifulSoup
 from datetime import datetime
 from ocd_backend.extractors import HttpRequestMixin
 from ocd_backend.items import BaseItem
 
-class CommonsItem(BaseItem, HttpRequestMixin):
-    namespaces = {
-        'oai': 'http://www.openarchives.org/OAI/2.0/',
-        'oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/',
-        'dc': 'http://purl.org/dc/elements/1.1/',
-        'doc': 'http://www.lyncode.com/xoai',
-        'xml': 'http://www.w3.org/XML/1998/namespace'
+class TropenMuseumItem(BaseItem, HttpRequestMixin):
+    media_mime_types = {
+        'png': 'image/png',
+        'jpg': 'image/jpg',
+        'tiff': 'image/tiff',
+        'tif': 'image/tiff'
     }
 
     def _get_text_or_none(self, xpath_expression):
-        node = self.original_item.find(xpath_expression, namespaces=self.namespaces)
+        node = self.original_item.find(xpath_expression)
         if node is not None and node.text is not None:
             return unicode(node.text)
 
@@ -23,49 +21,47 @@ class CommonsItem(BaseItem, HttpRequestMixin):
 
     # Get all node values for given path
     def _get_all_or_none(self, xpath_expression):
-        node = self.original_item.findall(xpath_expression, namespaces=self.namespaces)
+        node = self.original_item.findall(xpath_expression)
         if node is not None and len(node) > 0:
             items = []
             for item in node:
-                items.append(item.text)
+                items.append(item)
             return items
 
         return None
 
-    def _get_image_link(self):
-        original_id = self.get_original_object_id()
-        if re.match('\d+\-?\d+?$', original_id):
-            try:
-                image_width = int(self._get_text_or_none('.//meta/item[@name="ImageWidth"]'))
-            except TypeError, e:
-                image_width = 3072
-            try:
-                image_height = int(self._get_text_or_none('.//meta/item[@name="ImageLength"]'))
-            except TypeError, e:
-                image_height = 2304
-            return 'http://collectie.tropenmuseum.nl/ImagesHandler.ashx?image=%%5CG%%20schijf%%5CTMSMedia%%5Cimages%%5Cscreen%%5C%s.jpg&width=%s&height=%s' % (
-                original_id, image_width, image_height
-            )
-
     def get_original_object_id(self):
         filename = self._get_text_or_none('.//file/name')
-        print filename
         m = re.search('TMnr (\d+\-?\d+?)\.jpg$', filename)
         if m:
-            return m.group(1)
+            return m.group(1).decode('utf8')
         else:
             return filename
 
+    def get_description(self):
+        text = u''
+        description_html = self._get_all_or_none('.//description/language')
+
+        for desc in description_html:
+            soup = BeautifulSoup(desc.text)
+            filtered_text = soup.find(lang='nl')
+            if not filtered_text:
+                continue
+
+            filtered_text = filtered_text.findAll(text=True)
+            text += u' '.join([unicode(n.string).strip() for n in filtered_text if unicode(n.string) != 'Nederlands:'])
+
+        if text:
+            return text
+
+        return None
+
     def get_original_object_urls(self):
-        filename = self._get_text_or_none('.//file/name')
-        image_link = self._get_image_link()
-
         original_urls = {
-            'xml': 'http://tools.wmflabs.org/magnus-toolserver/commonsapi.php?image=%s&thumbwidth=150&thumbheight=150&versions&meta'% urllib.quote(filename)
+            'xml': 'http://tools.wmflabs.org/magnus-toolserver/commonsapi.php?image=%s'
+                    %  self._get_text_or_none('.//file/name'),
+            'html':  self._get_text_or_none('.//urls/description')
         }
-
-        if image_link:
-            original_urls['html'] = image_link
 
         return original_urls
 
@@ -82,29 +78,41 @@ class CommonsItem(BaseItem, HttpRequestMixin):
     def get_combined_index_data(self):
         combined_index_data = {}
 
-        title = self._get_text_or_none('.//file/name').replace('.jpg', '')
-
+        title = self._get_text_or_none('.//file/name').split('.')[0]
         combined_index_data['title'] = title
-        combined_index_data['description'] = title
+        combined_index_data['description'] = self.get_description()
 
         date = self._get_text_or_none('.//file/date')
-        combined_index_data['date'] = None
-        combined_index_data['date_granularity'] = 0
         if date:
-            date_match = re.search('datetime\=\"(\d+)\"', date)
-            if date_match:
-                combined_index_data['date'] = datetime.strptime(
-                    date_match.group(1),
-                    '%Y'
-                )
-                combined_index_data['date_granularity'] = 4
+            # Date is sometimes wrapped inside html...
+            html_date = re.search(r'datetime=\"(.*)\"', date)
+            if html_date:
+                print html_date.group(1)
+                date = html_date.group(1)
 
-        image_url = self._get_image_link()
-        if image_url:
-            combined_index_data['media_urls'] = [{
-              'original_url': image_url,
-              'content_type': 'image/jpg'
-            }]
+            date_formats = [
+                (r'^(\d{4})$', '%Y', 4),
+                (r'^(\d{4})-\d{4}$', '%Y', 4),
+                (r'^(\d{4}-\d{1,2})$', '%Y-%m', 6),
+                (r'^(\d{4}-\d{1,2}-\d{1,2})$', '%Y-%m-%d', 8),
+            ]
+
+            for d_regex, d_format, d_granularity in date_formats:
+                date_match = re.search(d_regex, date)
+                if date_match:
+                    combined_index_data['date'] = datetime.strptime(
+                        date_match.group(1), d_format)
+                    combined_index_data['date_granularity'] = d_granularity
+
+                    # We found a working format, stop trying alternatives
+                    break
+
+
+        img_file_url = self._get_text_or_none('.//urls/file')
+        combined_index_data['media_urls'] = [{
+          'original_url': img_file_url,
+          'content_type': self.media_mime_types[img_file_url.split('.')[-1].lower()]
+        }]
 
         return combined_index_data
 
@@ -114,17 +122,17 @@ class CommonsItem(BaseItem, HttpRequestMixin):
     def get_all_text(self):
         text_items = []
 
-        # Title
-        text_items.append(
-            self._get_text_or_none('.//file/name').replace('.jpg', '')
-        )
+        # # Title
+        title = self._get_text_or_none('.//file/name').split('.')[0]
+        text_items.append(title)
 
-        # Subjects
-        subjects = self._get_all_or_none('.//category')
-        if subjects:
-            text_items = text_items + subjects
+        # Categories
+        categories = self._get_all_or_none('.//categories/category')
+        if categories:
+            for category in categories:
+                text_items.append(category.text)
 
-        # Identifier
-        text_items.append(self.get_original_object_id())
+        # Description
+        text_items.append(self.get_description())
 
         return u' '.join([ti for ti in text_items if ti is not None])
