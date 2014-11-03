14de3914198206f9774cf4c5353d7d2258c25403
Bart de Goede <bart@dispectu.com>
2014-11-03 15:01:16 +0100

    List available backups, and an endpoint that serves backups for debugging purposes

--- ocd_frontend/wsgi.py
+++ ocd_frontend/wsgi.py
@@ -5,3 +5,13 @@ from ocd_frontend import rest
 application = DispatcherMiddleware(rest.create_app(), {
     '/v0': rest.create_app()
 })
+
+# For testing purposes, add a route that serves static files from a directory.
+# DO NOT USE IN PRODUCTION. Serve static files through your webserver instead.
+if application.app.config.get('DEBUG', False):
+    from flask import send_from_directory
+
+    @application.app.route('/data/<path:filename>')
+    def download_backup(filename):
+        return send_from_directory(application.app.config.get('BACKUP_DIR'),
+                                   filename, as_attachment=True)
