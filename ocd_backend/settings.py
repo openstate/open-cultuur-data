edd215c8db552a4513aebdf68a2eb3db911e5655
Bart de Goede <bart@dispectu.com>
2014-11-11 11:52:43 +0100

    Use msgpack-based custom serializer, as pickle is deprecated, and throws errors

--- ocd_backend/settings.py
+++ ocd_backend/settings.py
@@ -1,8 +1,19 @@
 import os
 
+# Register custom serializer for Celery that allows for encoding and decoding
+# Python datetime objects (and potentially other ones)
+from kombu.serialization import register
+from serializers import encoder, decoder
+
+register('ocd_serializer', encoder, decoder, content_encoding='binary',
+         content_type='application/ocd-msgpack')
+
 CELERY_CONFIG = {
     'BROKER_URL': 'redis://127.0.0.1:6379/0',
-    'CELERY_RESULT_BACKEND': 'redis://127.0.0.1:6379/0'
+    'CELERY_ACCEPT_CONTENT': ['ocd_serializer'],
+    'CELERY_TASK_SERIALIZER': 'ocd_serializer',
+    'CELERY_RESULT_SERIALIZER': 'ocd_serializer'
+    # 'CELERY_RESULT_BACKEND': 'redis://127.0.0.1:6379/0'
 }
 
 LOGGING = {
