501a090eea92e10c119e80642f753163510c3a98
Bart de Goede <bart@dispectu.com>
2014-11-11 13:49:36 +0100

    DummyLoader that prints data to stdout, for debugging purposes

--- ocd_backend/loaders/__init__.py
+++ ocd_backend/loaders/__init__.py
@@ -55,7 +55,13 @@ class ElasticsearchLoader(BaseLoader):
         log.info('Indexing documents...')
         elasticsearch.index(index=settings.COMBINED_INDEX, doc_type='item',
                             id=object_id, body=combined_index_doc)
-        elasticsearch.index(index='%s_%s' % (settings.DEFAULT_INDEX_PREFIX, self.source_definition['id']),
+
+        index_name = self.source_definition['id']
+        if 'index_name' in self.source_definition:
+            index_name = self.source_definition['index_name']
+
+        elasticsearch.index(index='%s_%s' % (settings.DEFAULT_INDEX_PREFIX,
+                                             index_name),
                             doc_type='item', id=object_id, body=doc)
 
         # For each media_urls.url, add a resolver document to the
@@ -73,3 +79,17 @@ class ElasticsearchLoader(BaseLoader):
                                          body=url_doc)
                 except ConflictError:
                     log.debug('Resolver document %s already exists' % url_hash)
+
+
+class DummyLoader(BaseLoader):
+    """
+    Prints the item to the console, for debugging purposes.
+    """
+    def load_item(self, object_id, combined_index_doc, doc):
+        print '=' * 50
+        print '%s %s %s' % ('=' * 4, object_id, '=' * 4)
+        print '%s %s %s' % ('-' * 20, 'combined', '-' * 20)
+        print combined_index_doc
+        print '%s %s %s' % ('-' * 20, 'doc', '-' * 25)
+        print doc
+        print '=' * 50
