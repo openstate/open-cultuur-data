1abcc5c16cf5f8bbefec868ea3a85d569dd317be
Bart de Goede <bart@dispectu.com>
2014-11-11 11:52:43 +0100

    Use msgpack-based custom serializer, as pickle is deprecated, and throws errors

---
+++ ocd_backend/serializers.py
@@ -0,0 +1,34 @@
+import datetime
+import msgpack
+
+
+def decode_datetime(obj):
+    if b'__datetime__' in obj:
+        obj = datetime.datetime.strptime(obj['as_str'], '%Y%m%dT%H:%M:%S.%f')
+    return obj
+
+
+def encode_datetime(obj):
+    if isinstance(obj, datetime.datetime):
+        return {'__datetime__': True, 'as_str': obj.strftime('%Y%m%dT%H:%M:%S.%f')}
+    return obj
+
+
+def encoder(obj):
+    """
+    Encode obj with msgpack, and use custom encoder for datetime objects
+
+    :param obj: value (dict) to encode
+    :return: binary document
+    """
+    return msgpack.packb(obj, default=encode_datetime)
+
+
+def decoder(obj):
+    """
+    Reverse of ``encode``; decode objects that was serialized using ``encoder``
+
+    :param obj: binary msgpack
+    :return: dict
+    """
+    return msgpack.unpackb(obj, object_hook=decode_datetime)
