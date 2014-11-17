170b236d9cb9fa5c7755396fa0724823bfd645e7
Bart de Goede <bart@dispectu.com>
2014-11-17 12:12:47 +0100

    Test layout, and tests for extractors and transformers

---
+++ tests/extractors/__init__.py
@@ -0,0 +1,22 @@
+import os.path
+from unittest import TestCase
+
+
+class ExtractorTestCase(TestCase):
+
+    def setUp(self):
+        self.PWD = os.path.dirname(__file__)
+        dump_path = os.path.abspath(os.path.join(self.PWD, '..', 'test_dumps/ocd_openbeelden_test.gz'))
+        self.source_definition = {
+            'id': 'test_definition',
+            'extractor': 'ocd_backend.extractors.staticfile.StaticJSONDumpExtractor',
+            'transformer': 'ocd_backend.transformers.BaseTransformer',
+            'item': 'ocd_backend.items.LocalDumpItem',
+            'loader': 'ocd_backend.loaders.ElasticsearchLoader',
+            'dump_path': dump_path,
+            'index_name': 'openbeelden'
+        }
+
+# Import test modules here so the noserunner can pick them up, and the
+# ExtractorTestCase is parsed. Add additional testcases when required
+from .staticfile import StaticfileExtractorTestCase
