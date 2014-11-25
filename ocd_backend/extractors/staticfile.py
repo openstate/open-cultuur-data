dc9ec79d632734260ebb7fbd3d91341db6873351
Breyten Ernsting <bje@dds.nl>
2014-11-25 14:25:08 +0100

    Implement Gemeente Ede as a source

--- ocd_backend/extractors/staticfile.py
+++ ocd_backend/extractors/staticfile.py
@@ -3,6 +3,7 @@ from ocd_backend.exceptions import ConfigurationError
 
 from click import progressbar
 import gzip
+import json
 from lxml import etree
 
 
@@ -73,9 +74,10 @@ class StaticXmlExtractor(StaticFileBaseExtractor):
 
         self.default_namespace = None
         if 'default_namespace' in self.source_definition:
-            self.default_namespace = self.source_definition['default_namespace']
-            
-        
+            self.default_namespace = self.source_definition[
+                'default_namespace'
+            ]
+
     def extract_items(self, static_content):
         tree = etree.fromstring(static_content)
 
@@ -95,6 +97,23 @@ class StaticXmlExtractor(StaticFileBaseExtractor):
             yield 'application/xml', etree.tostring(item)
 
 
+class StaticJSONExtractor(StaticFileBaseExtractor):
+    """
+    Extract items from JSON files.
+    """
+    def __init__(self, *args, **kwargs):
+        super(StaticJSONExtractor, self).__init__(*args, **kwargs)
+
+        if not self.source_definition.get('file_url'):
+            raise ConfigurationError('Missing \'file_url\' definition')
+
+    def extract_items(self, static_content):
+        static_json = json.loads(static_content)
+
+        for item in static_json:
+            yield 'application/json', json.dumps(item)
+
+
 class StaticJSONDumpExtractor(BaseExtractor):
     """
     Extract items from JSON dumps.
@@ -104,7 +123,6 @@ class StaticJSONDumpExtractor(BaseExtractor):
 
         if not self.source_definition.get('dump_path'):
             raise ConfigurationError('Missing \'dump_path\' definition')
-
 
     def run(self):
         """
@@ -116,7 +134,8 @@ class StaticJSONDumpExtractor(BaseExtractor):
             yield item
 
     def extract_items(self, dump_path):
-        with progressbar(gzip.open(dump_path, 'rb'), label='Loading %s' %
-                dump_path) as f:
+        with progressbar(
+            gzip.open(dump_path, 'rb'), label='Loading %s' % dump_path
+        ) as f:
             for line in f:
                 yield 'application/json', line.strip()
