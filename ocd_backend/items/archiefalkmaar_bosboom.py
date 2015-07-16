9c99368f84f52aa076e1d28087f83d84f7012fbf
Breyten Ernsting <bje@dds.nl>
2015-07-16 12:21:13 +0200

    Right name also.

--- ocd_backend/items/archiefalkmaar_bosboom.py
+++ ocd_backend/items/archiefalkmaar_bosboom.py
@@ -3,4 +3,4 @@ from ocd_backend.items.archiefalkmaar_baseItem import ArchiefAlkmaarBaseItem
 
 class ArchiefAlkmaarBosboomItem(ArchiefAlkmaarBaseItem):
     def get_collection(self):
-        return u'Reg Archief Alkmaar Bosboom'
+        return u'Regionaal Archief Alkmaar Bosboom'
