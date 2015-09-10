e4af82e5f3bfb0bd9668c0a7c2eeb0ee873218f0
Breyten Ernsting <bje@dds.nl>
2015-09-10 15:15:50 +0200

    FFmpeg should probably be at 2.4

--- install_pyav_deps.sh
+++ install_pyav_deps.sh
@@ -31,7 +31,7 @@ checkinstall --pkgname=fdk-aac --pkgversion="0.1.0" --backup=no \
   --deldoc=yes --fstrans=no --default
 
 cd ..
-git clone --depth 1 https://chromium.googlesource.com/webm/libvpx 
+git clone --depth 1 https://chromium.googlesource.com/webm/libvpx
 cd libvpx
 ./configure --enable-shared
 make
@@ -55,8 +55,11 @@ make
 make install
 
 cd ..
-git clone --depth 1 git://source.ffmpeg.org/ffmpeg
-cd ffmpeg
+#git clone --depth 1 git://source.ffmpeg.org/ffmpeg
+git clone git@github.com:FFmpeg/FFmpeg.git
+cd FFmpeg
+git co release/2.4
+
 ./configure --enable-shared --enable-gpl --enable-libfaac --enable-libmp3lame --enable-libopencore-amrnb \
   --enable-libopencore-amrwb --enable-librtmp --enable-libtheora --enable-libvorbis \
   --enable-libx264 --enable-nonfree --enable-version3  --enable-libxvid
