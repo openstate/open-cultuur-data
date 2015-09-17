7438fef61a4390d613e57ab62fbdc45e447d353e
Breyten Ernsting <bje@dds.nl>
2015-09-17 12:30:26 +0200

    Oops, circular import do not work. See #120

--- ocd_backend/enrichers/media_enricher/__init__.py
+++ ocd_backend/enrichers/media_enricher/__init__.py
@@ -12,8 +12,6 @@ from ocd_backend.log import get_source_logger
 
 from .tasks import ImageMetadata, MediaType
 
-from .nabeeldbank import NationaalArchiefEnricher
-
 log = get_source_logger('enricher')
 
 
@@ -177,3 +175,18 @@ class MediaEnricher(BaseEnricher):
         enrichments['media_urls'] = media_urls_enrichments
 
         return enrichments
+
+
+class NationaalArchiefEnricher(MediaEnricher):
+    def enrich_item(self, enrichments, object_id, combined_index_doc, doc):
+        # take only the first media item (or the first that is of a high resolution)
+        # since enriching them all takes way too long
+        media_item = doc['media_urls'][0]
+
+        if doc.has_key('media_urls'):
+            large_media_items = [m for m in doc['media_urls'] if u'/1280x' in m['original_url']]
+            if len(large_media_urls) > 0:
+                media_item = large_media_items[0]
+
+        return super(NationaalArchiefEnricher, self).enrich_item(
+            enrichments, object_id, combined_index_doc, {'media_urls': [media_item]})
