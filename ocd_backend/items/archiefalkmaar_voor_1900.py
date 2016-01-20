7cb0becd3ac561ea9f9e4ee78cfca37e215f8c5b
Breyten Ernsting <bje@dds.nl>
2016-01-20 15:13:46 +0100

    Add Alkmaar before 1900 as a source

---
+++ ocd_backend/items/archiefalkmaar_voor_1900.py
@@ -0,0 +1,6 @@
+from ocd_backend.items.archiefalkmaar_baseItem import ArchiefAlkmaarBaseItem
+
+
+class ArchiefAlkmaarBosboomItem(ArchiefAlkmaarBaseItem):
+    def get_collection(self):
+        return u'Regionaal Archief Alkmaar Voor 1900'
