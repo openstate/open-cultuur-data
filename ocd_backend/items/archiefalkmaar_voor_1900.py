f78e808f302e723ade73adce756477fa6edca59a
Breyten Ernsting <bje@dds.nl>
2016-01-27 11:56:08 +0100

    Oops, wrong class name there.

--- ocd_backend/items/archiefalkmaar_voor_1900.py
+++ ocd_backend/items/archiefalkmaar_voor_1900.py
@@ -1,6 +1,6 @@
 from ocd_backend.items.archiefalkmaar_baseItem import ArchiefAlkmaarBaseItem
 
 
-class ArchiefAlkmaarBosboomItem(ArchiefAlkmaarBaseItem):
+class ArchiefAlkmaarVoor1900Item(ArchiefAlkmaarBaseItem):
     def get_collection(self):
         return u'Regionaal Archief Alkmaar Voor 1900'
